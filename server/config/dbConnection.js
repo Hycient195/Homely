85c6bf01f13356960294aa1cfbd7e68b73e4df0e
Hycient Onyeukwu <onyeukwuhycient@gmail.com>
2022-01-01 20:31:45 +0100

    new commit

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -5,8 +5,8 @@ module.exports.dbConnection = () =>{
     mongoose.connect(process.env.MONGO_DB_URL,{
         useNewUrlParser : true,
         useUnifiedTopology : true,
-        useCreateIndex : true,
-        useFindAndModify : false
+        // useCreateIndex : true,
+        // useFindAndModify : false
     })
 
     .then(()=>{
